5886485cfd72d0133112741d3dcfe7cd1d891d37
Elena 879 <elena.zabolotnaya18@gmail.com>
2017-12-23 20:22:19 +0200

    add checkbox

--- source-js/main.js
+++ source-js/main.js
@@ -1,18 +1,33 @@
-jQuery(document).ready(function (event) {
-  jQuery('.add-task').on('click', function () {
+jQuery(document).ready(function () {
+  jQuery('.add-task').on('click', addItem)
+  jQuery('.todo-container').on('change','.checkbox', completeItem)
+  jQuery('.todo-container').on('click', '.delete', deleteItem)
+
+  jQuery('.input-task').on('keypress', function(event) {
+    if (event.which === 13) {
+      addItem();
+      event.preventDefault()
+    }
+  })
+
+  function addItem(event) {
     var valueTask = jQuery('.input-task').val()
     if (!valueTask) {
       alert('Please, enter your text')
       return false
     }
 
-    jQuery('.todo-container').append("<li> <span>" + valueTask + "</span> <button class='delete'>×</button><button class='change'>change</button></li>")
+    var appendValue = ("<li><span>" + valueTask + "</span><input type='checkbox' class='checkbox'><button class='delete'>×</button></li>")
+    jQuery('.todo-container').append(appendValue)
     jQuery('.input-task').val('')
+  }
 
 
-    jQuery(".delete").on('click', function () {
-      jQuery(this).parent().remove()
-    })
-  })
+  function deleteItem(event) {
+    jQuery(this).parent().remove()
+  }
+
+  function completeItem(event) {
+    jQuery(this).parent().toggleClass('done')
+  }
 })
-
